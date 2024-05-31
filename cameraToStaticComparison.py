629cb581248f7df09b669ae968ae7347bffb56d9
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2024-06-01 01:54:10 +0200

    fullscreen and multiple players added

--- cameraToStaticComparison.py
+++ cameraToStaticComparison.py
@@ -7,8 +7,10 @@ import numpy as np
 from poseDetection import MoveNetDetector, Person
 from DifferenceCalculator import DifferenceCalculator
 from ffpyplayer.player import MediaPlayer
-offset = -0.3#todo some slider or sth
+
+offset = -0.3  # todo some slider or sth
 camera_buffer_size = 5
+
 # Open the default camera
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
@@ -18,62 +20,58 @@ video = cv2.VideoCapture(video_path)
 
 detector = MoveNetDetector(verbose=False)
 
-outputPerson: List[Person] = []
+outputPersons: List[Person] = []
 frame_counter = 0
-diff_value = 0
+scores = []
 
 def compare_frames(frame_video):
-    global diff_value  # Declare diff_value as global to modify it within the function
-    global outputPerson
+    global scores  # Declare scores as global to modify it within the function
+    global outputPersons
 
     for i in range(camera_buffer_size):
         ret_cam, frame_cam = cap.read()
     ret_cam, frame_cam = cap.read()
     frame_cam = cv2.flip(frame_cam, 1)
 
-    # cv2.imwrite(f'compare/{frame_counter}frame_cam.jpg', frame_cam)
-    # cv2.imwrite(f'compare/{frame_counter}frame_video.jpg', frame_video)
-
     # Detect and plot the results for the camera frame
-    outputPerson = detector.detect_and_plot(frame_cam, plot=False)
+    outputPersons = detector.detect_and_plot(frame_cam, plot=False)
 
     # Detect and plot the results for the video frame
     outputVideo = detector.detect_and_plot(frame_video, plot=True)
 
-    if len(outputPerson) > 0 and len(outputVideo) > 0:
-        diff_value = DifferenceCalculator.total_difference(outputPerson[0].keypoints, outputVideo[0].keypoints)
-        print(f"Difference: {diff_value}")
+    if len(outputPersons) > 0 and len(outputVideo) > 0:
+        for i in range(min(len(outputPersons), len(scores))):
+            diff_value = DifferenceCalculator.total_difference(outputPersons[i].keypoints, outputVideo[0].keypoints)
+            scores[i] += diff_value*10
+        if len(outputPersons) > len(scores):
+            for i in range(len(scores), len(outputPersons)):
+                diff_value = DifferenceCalculator.total_difference(outputPersons[i].keypoints, outputVideo[0].keypoints)
+                scores.append(diff_value*10)
+        elif len(outputPersons) < len(scores):
+            scores = scores[:len(outputPersons)]
+        print(f"Scores: {scores}")
 
-
-video=cv2.VideoCapture(video_path)
+video = cv2.VideoCapture(video_path)
 fps = video.get(cv2.CAP_PROP_FPS)
 frame_counter = 0
 start_time = time.time()
 player = MediaPlayer(video_path)
 
+# Create a named window and set it to fullscreen
+cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('Video', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 while True:
     ret_video, frame_video = video.read()
 
-    # Display the keypoints on the video frame
-    if len(outputPerson) > 0:
-        keypoints = outputPerson[0].keypoints
-        height, width, _ = frame_video.shape
-        for keypoint in keypoints:
-            x, y, probability = keypoint
-            x = int(x * width)
-            y = int(y * height)
-            cv2.circle(frame_video, (x, y), 5, (0, 255, 0), -1)
-
-    # Display the difference value on the video frame
-    text = f"Difference: {diff_value}"
-    cv2.putText(frame_video, text, (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 2, cv2.LINE_AA)
-
-
+    # Display the scores on the video frame
+    for i in range(min(len(scores), 6)):
+        text = f"Score {i + 1}: {int(scores[i])}"
+        cv2.putText(frame_video, text, (100, 300 + i * 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
     # Display the video frame
     cv2.imshow('Video', frame_video)
     if frame_counter % 15 == 0:
-
         if not ret_video:
             break
         thread = threading.Thread(target=compare_frames, args=(frame_video,))
@@ -85,12 +83,10 @@ while True:
 
     displayed_frames_time = frame_counter / fps
 
-    TIME_TO_SLEEP_TO_MATCH_AUDIO = max(0.001, displayed_frames_time-elapsed_time-offset)
+    TIME_TO_SLEEP_TO_MATCH_AUDIO = max(0.001, displayed_frames_time - elapsed_time - offset)
     # Press 'q' to quit
     if cv2.waitKey(int((TIME_TO_SLEEP_TO_MATCH_AUDIO) * 1000)) & 0xFF == ord('q'):
         break
-
-
 
 # Release the camera, video, and close all windows
 cap.release()
